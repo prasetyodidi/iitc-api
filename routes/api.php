5c55fd455ca410b6cb65e8f7c30807d3d6722b08
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-06-20 01:01:46 +0700

    Route Register

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('login', [LoginController::class, 'store'])->name('login');
+Route::post('register', [RegisterController::class, 'store'])->name('register');
