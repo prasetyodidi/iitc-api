9d48f7920745e0fa64087ef0f9eed210ea9cec1a
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-04 10:56:22 +0700

    feat:create and define teams migration

---
+++ database/migrations/2023_07_04_033001_create_teams_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('leader_id');
+            $table->unsignedBigInteger('competition_id');
+            $table->string('name');
+            $table->string('title');
+            $table->string('avatar');
+            $table->timestamps();
+
+            $table->foreign('leader_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('competition_id')
+                ->references('id')
+                ->on('competitions')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
