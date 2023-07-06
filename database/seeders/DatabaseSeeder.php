ac7ba61a97815a8ca85eefe686e1651cee484113
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-06 08:31:29 +0700

    feat:seeding members

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Member;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -25,15 +26,24 @@ class DatabaseSeeder extends Seeder
             CompetitionSeeder::class,
             CategoryCompetitionSeeder::class
         ]);
-        $users = User::factory(100)->create();
 
+        $users = User::factory(100)->create();
         foreach ($users as $user) {
-            $isJoin = fake()->boolean;
-            if ($isJoin) {
-                Team::factory()->create([
-                    'leader_id' => $user->id,
-                    'competition_id' => fake()->numberBetween(1, 10),
+            Team::factory()->create([
+                'leader_id' => $user->id,
+                'competition_id' => fake()->numberBetween(1, 10),
+            ]);
+        }
+
+        $members = User::factory(300)->create();
+        $memberIndex = 0;
+        for ($i=1; $i <= 100; $i++) {
+            for ($j=0; $j < 3; $j++) {
+                Member::factory()->create([
+                    'team_id' => $i,
+                    'user_id' => $members[$memberIndex]->id,
                 ]);
+                $memberIndex++;
             }
         }
     }
