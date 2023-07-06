ab3ea8d4fd5a39140d8ec9c21ba7b13eda8ef127
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-06 08:31:52 +0700

    feat:join team controller

--- app/Http/Controllers/JoinTeamController.php
+++ app/Http/Controllers/JoinTeamController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\LeaderJoinOwnTeamException;
+use App\Http\Requests\StoreJoinTeamRequest;
+use App\Models\Member;
+use App\Models\Team;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Spatie\FlareClient\Http\Exceptions\NotFound;
 
 class JoinTeamController extends Controller
 {
-    //
+    public function store(StoreJoinTeamRequest $request, string $teamId): JsonResponse
+    {
+        try {
+            $team = Team::query()->findOrFail($teamId);
+            $user = auth()->user();
+            $code = $request->input('code');
+
+            // code not found
+            if ($code != $team->code) {
+                throw new NotFound('team code not found');
+            }
+
+            // leader joined his own team
+            if ($team->leader_id == $user->id) {
+                throw new LeaderJoinOwnTeamException('you are the leader!');
+            }
+
+            $user->asMembers()->syncWithoutDetaching($team->id);
+
+            $responseData = [
+                'status' => 1,
+                'message' => 'Succeed joined a team',
+            ];
+
+            return response()->json($responseData, 200);
+        } catch (Exception $exception) {
+            $responseData = [
+                'status' => 0,
+                'message' => $exception->getMessage(),
+            ];
+
+            return response()->json($responseData, 400);
+        }
+    }
 }
