2e21a19b71a1b3b86286c88a06bbb80cc4bb4f08
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-07 08:09:52 +0700

    feat:create participant migration

---
+++ database/migrations/2023_07_06_202938_create_participants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->uuid('user_id')->unique();
+            $table->string('grade');
+            $table->string('gender');
+            $table->string('student_id_number');
+            $table->string('institution');
+            $table->string('photo_identity');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};
