0067bdf12f5389d67c69f7e867f9a02bcf52ca56
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-26 16:00:02 +0700

    update documentation

--- app/Http/Requests/StoreCompetitionRequest.php
+++ app/Http/Requests/StoreCompetitionRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCompetitionRequest extends FormRequest
 {
@@ -17,7 +18,7 @@ class StoreCompetitionRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, Rule|array|string>
      */
     public function rules(): array
     {
