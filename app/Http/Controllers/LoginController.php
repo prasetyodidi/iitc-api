176cdbfa8baeffa7eeca156fd0e13490b83a46be
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-07-30 12:27:21 +0700

    fix:error handling

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,34 +12,20 @@ class LoginController extends Controller
 {
     public function store(StoreLoginRequest $request)
     {
-        try {
-            $user = User::where("email", $request->email)->firstOrFail();
-            if (!Hash::check($request->password, $user->password)) {
-                throw new AuthenticationException("Invalid Password");
-            }
-
-            $token = $user->createToken('authToken')->plainTextToken;
-
-            $data = [
-                "status" => 1,
-                "message" => "berhasil login",
-                "data" => [
-                    "access_token" => $token
-                ]
-            ];
-            return response()->json($data, 200);
-        }catch (AuthenticationException $exception) {
-            $data = [
-                "status" => 0,
-                "message" => "wrong password",
-            ];
-            return response()->json($data, 401);
-        }catch (Exception $exception) {
-            $data = [
-                "status" => 0,
-                "message" => $exception->getMessage(),
-            ];
-            return response()->json($data, 404);
+        $user = User::where("email", $request->email)->firstOrFail();
+        if (!Hash::check($request->password, $user->password)) {
+            throw new AuthenticationException("Invalid Password");
         }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $data = [
+            "status" => 1,
+            "message" => "berhasil login",
+            "data" => [
+                "access_token" => $token
+            ]
+        ];
+        return response()->json($data, 200);
     }
 }
